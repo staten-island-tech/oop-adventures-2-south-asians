e5cbe902725264ef03055480760478dc0d5bb228
imaduck25 <fatimaa158@nycstudents.net>
2026-01-07 06:32:46 -0800

    Create items.py

---
+++ items.py
@@ -0,0 +1,11 @@
+class item:
+    def __init__(self, object, scarcity):
+        self.object = object
+        self.scarcity = scarcity
+
+        class food:
+            def __init__(self, energy = 5):
+                self.energy = energy
+
+
+ite
