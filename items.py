74dd2085e58175c430e0b8f9faa962ac6f2824ad
imaduck25 <fatimaa158@nycstudents.net>
2026-01-12 06:44:26 -0800

    Update items.py

--- items.py
+++ items.py
@@ -1,18 +1,56 @@
-class amk:
-    #anchor obj, map frag, keycards
-    def __init__(self, name, number, total, des):
-        self.name= name
-        self.number = number
-        self.total = total
-        self.des= des
-
-class landf:
-#Lantern and food
+class item:
     def __init__(self, name, des):
         self.name = name
-        self.des =des
+        self.des = des
 
-AnchorObj= amk(f"Anchor Object", "You have {self.number} anchor object(s).", "There are {self.total} left to find.", "Anchor Objects keep the world from rearranging itself.")
-MapFrag = amk(f"Map Fragment", "You have {self.number} map fragemet(s).", "There are {self.total} left to find")
+class Collectible(item):
+#anchor obj, map frag, keycards
+#item parnet class, collectible child clas
+    def __init__(self, name, total, total_needed, des):
+        super().__init__(name, des)
+        self.count = 0
+        self.total = total_needed
 
-print(AnchorObj)
+    def update(self):
+        return f"You have {self.count} {self.name}(s). There are {self.total} left to find."
+
+
+class Lantern(item):
+    def __init__(self, name, des):
+        super().__init__("Lantern", "A lamp that reveals hidden things in the In-Between.")
+
+class Food(item):
+    def __init__(self, name, heal_amnt = 15):
+        super().__init__(name, "Food item.")
+        self.heal_amnt = heal_amnt
+
+def pick_up_collectible(player, item):
+    item.count +=1
+    item.total -=1
+    player.inventory.append(item)
+    print(item.update())
+
+AnchorObj = Collectible(
+    "Anchor Object",
+    "Anchor Objects keep the world from rearranging itself.",
+    3
+)
+
+Apple = Food("Apple", heal_amnt=10)
+Crackers = Food("Crackers", heal_amnt=5)
+EnergyBar = Food("Energy Bar", heal_amnt=20)
+RottenSnack = Food("Snack", heal_amnt=-10)
+
+MapFragment = Collectible(
+    "Map Fragment",
+    "A torn piece of the floor's layout.",
+    5
+)
+
+Keycard = Collectible(
+    "Keycard",
+    "A security card that unlocks restricted doors.",
+    5
+)
+
+print(Keycard)
