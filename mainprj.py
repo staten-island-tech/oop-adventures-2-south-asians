19fc3edb4b0b539058e5778c519e4525d0b2feb5
medhavenkatraj <medhav@nycstudents.net>
2026-01-12 10:08:25 -0500

    1/12

---
+++ mainprj.py
@@ -0,0 +1,7 @@
+from characters import Npc, You, Monster
+from items import Lantern, pick_up_collectible, AnchorObj, MapFragment, Keycard, Apple, Crackers, EnergyBar, RottenSnack
+
+
+player1 = You("Medha", 6)
+
+
