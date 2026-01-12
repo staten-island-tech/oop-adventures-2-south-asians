9cf2ef80d8d0367f2c173fdedbf14fa6497bcc2c
imaduck25 <fatimaa158@nycstudents.net>
2026-01-12 07:08:19 -0800

    Update mainprj.py

---
+++ mainprj.py
@@ -0,0 +1,6 @@
+""" from items import Lantern, pick_up_collectible, AnchorObj, MapFragment, Keycard, Apple, Crackers, EnergyBar, RottenSnack
+from characters import Npc, Monster, You """
+
+print("You live on the sixth floor of your apartment building, and the elevator has always paused strangely between floors five and six. ")
+print("One night, halfway through that familiar slowdown, you press the button for five again just to see what happens. The elevator shudders, the lights flicker, and the doors slide open to a floor that shouldn’t exist. The sign beside the doorway reads 5½.")
+print("You step out of the elevator without thinking, expecting the usual hallway. The doors close behind you before you even turn around./n")
