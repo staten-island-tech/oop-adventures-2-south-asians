063cb3588cc980ab12a0853855ef7b140b40767c
imaduck25 <fatimaa158@nycstudents.net>
2026-01-14 07:06:07 -0800

    Update mainprj.py

--- mainprj.py
+++ mainprj.py
@@ -1,10 +1,16 @@
-from items import Lantern
+""" from characters import You
+
+player=You() """
 
 print("You live on the sixth floor of your apartment building, and the elevator has always paused strangely between floors five and six. ")
 print("One night, halfway through that familiar slowdown, you press the button for five again just to see what happens. The elevator shudders, the lights flicker, and the doors slide open to a floor that shouldn’t exist.")
 print(" ")
-print("You step out of the elevator without thinking, expecting the usual hallway. The doors close behind you before you even turn around.")
+print("You step out of the elevator without thinking, expecting the usual hallway. The doors close behind you before you even turn around.", "\nWhen you do, the wall is smooth. No call button. No seam. No elevator. The display above the empty space reads: 5½.")
 print("When you do, the wall is smooth. No call button. No seam. No elevator. The display above the empty space reads: 5½.")
 print("The hallway looks almost familiar, but wrong. The lights hum unevenly. The air feels heavy. Every apartment door is cracked open just enough to suggest someone might be watching from inside.")
 print(" ")
-print("A br")
+print("A bright red Post-It note lies on the floor.")
+print("“Don’t wander. It notices movement.”")
+print(" ")
+print("A few steps ahead, a folded sheet of paper lies on the ground. The handwriting is rushed.")
+print("")
